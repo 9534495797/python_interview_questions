6856f263af1da3cef47aa1abc3484eea7aa06798
9534495797 <chaubeyankit927@gmail.com>
2023-02-05 18:56:54 +0530

    ok

---
+++ arraylargestsmallest.py
@@ -0,0 +1,19 @@
+# arr = [10, 89, 9, 56, 4, 80, 8]
+# mini = arr[0]
+# maxi = arr[0]
+
+# for i in range(len(arr)):
+#   if arr[i] < mini: mini = arr[i] 
+  
+# if arr[i] > maxi: maxi = arr[i]
+
+# print (mini)
+# print (maxi)
+
+
+
+arr = [10, 89, 9, 56, 4, 80, 8]
+arr.sort()
+
+print (arr[0])
+print (arr[-1])
