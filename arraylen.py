114f508e57117f96ceb794a9af89e35e0127c816
9534495797 <chaubeyankit927@gmail.com>
2023-02-06 19:49:36 +0530

    pk

---
+++ arraylen.py
@@ -0,0 +1,9 @@
+# Give the list as static input and store it in a variable.
+gvn_lst = [1, 8, 15, 25, 6, 2]
+# Calculate the length of the given list using the len() function and
+# store it in another variable.
+lst_len = len(gvn_lst)
+# Print the given List
+print("The given List =  ", gvn_lst)
+# Print the length of the given list
+print("The given list's length = ",lst_len)
