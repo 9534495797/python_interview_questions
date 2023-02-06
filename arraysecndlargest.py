880fc8222e6df5315a8724505c9c42dbc63b4de5
9534495797 <chaubeyankit927@gmail.com>
2023-02-06 19:51:57 +0530

    ok

---
+++ arraysecndlargest.py
@@ -0,0 +1,7 @@
+#program to find the second largest number of list
+# declaring the list
+list_val = [20, 30, 40, 25, 10]
+# sorting the list
+list_val.sort ()
+#displaying the second last element of the list
+print ("The second largest element of the list is:", list_val [-2])
