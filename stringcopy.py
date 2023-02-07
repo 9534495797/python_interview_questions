5d724215ab516f02f789ff6a5e55f82d7b166cdd
9534495797 <chaubeyankit927@gmail.com>
2023-02-08 04:25:32 +0530

    ok

---
+++ stringcopy.py
@@ -0,0 +1,10 @@
+# Python Program to Copy a String to Another String
+
+# Take the Input From the User
+str1 = input("Enter The String: ")
+
+str2 = str1
+str3 = str1[:]
+
+print("The Final String : Str2  = ", str2)
+print("The Final String : Str3  = ", str3)
